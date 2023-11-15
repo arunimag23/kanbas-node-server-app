eacfea9aaa3b0ed36d74a8a9154276ec19ea65a8
arunimag23 <emailarunimagupta@gmail.com>
2023-11-14 21:58:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import Hello from "./hello.js";
+import cors from "cors";
+import Lab5 from "./lab5.js";
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from './assignments/routes.js';
+import "dotenv/config";
+
+const app = express()
+app.use(express.json());
+app.use(cors({    
+    credentials: true,
+    origin: process.env.FRONTEND_URL}));
+ModuleRoutes(app);
+Hello(app);
+Lab5(app);
+CourseRoutes(app);
+AssignmentRoutes(app);
+app.listen(process.env.PORT || 4000);
